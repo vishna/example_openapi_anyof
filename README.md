bc7a7a971b4ba5bf2cfc878912508d8da48844d4
Lukasz Wisniewski <luke.cherrish@gmail.com>
2021-03-13 11:58:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# OpenAPI List<anyOf> support for Dart
+
+This example shows anyOf support not working with standard implementation and working with my experimental branch
+
+
+Run
+
+```
+./generate-api.rb
+```
+
+to generate standard broken implementation
+
+Run
+
+```
+./generate-api.rb dev
+```
+
+to generate implementation with anyOf fix
---
+++ generate-api.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+
+JAR_CACHE_DIR = ".jarCache"
+OPEN_API_SCHEMA = "anyOf.yaml"
+TARGET_DIR = "generated_lib"
+LIB_NAME= "generated_lib"
+
+`mkdir -p #{JAR_CACHE_DIR}`
+
+class Jar
+  attr_reader :group, :artifactId, :version
+
+  def initialize(group, artifactId, version)
+    @group = group
+    @artifactId = artifactId
+    @version = version
+  end
+
+  def filename
+  	"#{artifactId}-#{version}.jar"
+  end
+
+  def downloadUrl
+  	"https://repo1.maven.org/maven2/#{group.gsub('.', '/')}/#{artifactId}/#{version}/#{filename}"
+  end
+
+  def ensure
+  	target = localPath
+  	if !File.file?(target) then
+  		puts "Downloading #{filename}"
+  		`wget -U "Any User Agent" -O #{target} #{downloadUrl}`
+  	end
+  end
+
+  def localPath
+  	"#{JAR_CACHE_DIR}/#{filename}"
+  end
+end
+
+def repoBuild
+  `rm -rf dart-openapi-maven`
+  `git clone git@github.com:vishna/dart-openapi-maven.git`
+  `cd dart-openapi-maven && git checkout anyof_support`
+  `cd dart-openapi-maven && mvn package`
+  `cp dart-openapi-maven/target/openapi-dart-generator-4.3-SNAPSHOT.jar #{JAR_CACHE_DIR}/dev.jar`
+  `rm -rf dart-openapi-maven`
+end
+
+dartgen = Jar.new("com.bluetrainsoftware.maven", "openapi-dart-generator", "4.2")
+openapi = Jar.new("org.openapitools", "openapi-generator-cli", "5.0.0")
+
+dartgen.ensure
+openapi.ensure
+
+openapi_cli = "java -cp #{openapi.localPath}:#{dartgen.localPath} org.openapitools.codegen.OpenAPIGenerator"
+
+# pass dev parameter to checkout repo and build jar locally
+if ARGV[0] == "dev" then
+  repoBuild()
+  openapi_cli = "java -cp #{openapi.localPath}:#{JAR_CACHE_DIR}/dev.jar org.openapitools.codegen.OpenAPIGenerator"
+end
+
+# cleanup
+`rm -rf #{TARGET_DIR}`
+
+# generate
+puts `#{openapi_cli} generate --enable-post-process-file -i #{OPEN_API_SCHEMA} -g dart2-api --output "#{TARGET_DIR}" --additional-properties "pubName=#{LIB_NAME}"`
+
+# force update & pretty formatting
+puts `cd #{TARGET_DIR} && flutter pub get`
+puts `dartfmt -w #{TARGET_DIR}`
